d9dc42ec8857f74cb316dc40ff0c3f2aa4ef02dd
Warunyou140845 <122431926+Warunyou140845@users.noreply.github.com>
2023-01-11 14:11:53 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+document.getElementById("Calculator1").addEventListener("click",function(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1)+parseInt(num2);
+    document.getElementById("result").innerHTML = "result :" + sum
+    alert(sum)
+});
+
+document.getElementById("Calculator2").addEventListener("click",function(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1)-parseInt(num2);
+    document.getElementById("result").innerHTML = "result :" + sum
+    alert(sum)
+});
+
+document.getElementById("Calculator3").addEventListener("click",function(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1)*parseInt(num2);
+    document.getElementById("result").innerHTML = "result :" + sum
+    alert(sum)
+});
+
+document.getElementById("Calculator4").addEventListener("click",function(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    var sum = parseInt(num1)/parseInt(num2);
+    document.getElementById("result").innerHTML = "result :" + sum
+    alert(sum)
+});
+function myalert(){
+    alert(sum)
+}
